4d6272c695a80ecd6ae580266d69cc8037c6d5d0
Luc Didry <ldidry@cpan.org>
2013-10-26 02:51:54 +0000

    Import of LDIDRY/App-padconsole-0.06 from CPAN.
    gitpan-cpan-distribution: App-padconsole
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         LDIDRY/App-padconsole-0.06.tar.gz
    gitpan-cpan-author:       LDIDRY
    gitpan-cpan-maturity:     released

--- test.pl
+++ test.pl
@@ -3,9 +3,10 @@
 use warnings;
 use strict;
 
-use Test::More tests => 5;
+use Test::More tests => 6;
 use_ok( 'Etherpad::API' );
 use_ok( 'Term::ReadLine' );
 use_ok( 'Config::YAML' );
 use_ok( 'URI::Escape' );
 use_ok( 'DateTime' );
+use_ok( 'Browser::Open' );
